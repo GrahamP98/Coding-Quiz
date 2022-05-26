8a0010c92805ae8fe9512c722592089ca06f97f1
GrahamP98 <g.purnell15@gmail.com>
2022-05-26 16:24:21 -0400

    timer isnt working and questions arent popping up

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
 // Quiz state variables
 var questionIndex = 0;
-var state = 'start';
+
 // DOM referenced variables
 var startsecEl = document.querySelector("#startsec");
 var quizsecEl = document.querySelector("#quizsec");
 var endsecEl = document.querySelector("#endsec");
 var startBtn = document.querySelector("#startbtn");
 var submitBtn = document.querySelector("#submit")
-var quizTitle = document.querySelector("#q-title");
 var questionsEl = document.querySelector("#questions");
 var choicesEl = document.querySelector("#choices");
+var initialsEl = document.querySelector("#initials");
+
 
 // Timer specific variables
 var timeEl = document.querySelector("#time")
-var time = questions.length * 15;
+var time = questionsEl.length * 15;
 var timerId;
 
+// Questions for coding quiz with choices and correct answer.
+var questions = [
+    {
+        question: "Object properties are made up of ____.",
+        choices: ["Key pairs", "Functions", "This statements", "All of the above"],
+        answer: "Key pairs"
+    },
+    {
+        question: "What does CSS stand for?",
+        choices: ["Complicated Styling Selector", "Cool Style Selections", "Cascading Style Sheets", "Cascading Style Selector"],
+        answer: "Cascading Style Sheets"
+    },
+    {
+        question: "The concat() method is used to merge two or more ____.",
+        choices: ["Strings", "Arrays", "Booleans", "All of the above"],
+        answer: "Arrays"
+    },
+    {
+        question: ".split in javascript takes the given ____ and puts it into a(n) ____.",
+        choices: ["String, Array", "Array, String", "Boolean, Table", "String, Variable"],
+        answer: "String, Array"
+    },
+    {
+        question: "Media quiries need to be put on ____ of CSS page.",
+        choices: ["Top","Middle","Bottom","Anywhere, it doesnt matter"],
+        answer: "Bottom"
+    }
+]
 
 function quizStart() {
 
@@ -35,7 +64,7 @@ function tick() {
     timeEl.textContent = time;
 
     if (time <= 0) {
-        endsecEl();
+        endQuiz();
     }
 }
 
@@ -62,7 +91,9 @@ function getQuestion() {
 }
 
 function questionClick() {
+
     if (this.value !== questions[questionIndex].answer) {
+       
         time -= 15;
 
         if (time < 0) {
@@ -80,4 +111,18 @@ function questionClick() {
         }
     }
 }
+ 
+function endQuiz() {
+
+    clearInterval(timerId);
+
+    var userendsecEl = document.querySelector("#endsec");
+    userendsecEl.setAttribute("class", "visible");
+
+    var userscoreEl = document.querySelector("#userscore");
+    userscoreEl.textContent = time;
+
+    quizsecEl.setAttribute("class", "ghost");
+}
+
 startBtn.onclick = quizStart;
