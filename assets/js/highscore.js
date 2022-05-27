8aefdb968ff2e5389a176238b9296e07f2ed4ff5
GrahamP98 <g.purnell15@gmail.com>
2022-05-26 20:24:30 -0400

    everything is done besides read me

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,12 +1,13 @@
+// declared variables
 var highScore = document.querySelector("#highScore");
 var clear = document.querySelector("#clear");
 var goBack = document.querySelector("#goBack");
-
+// if clear is clicked it clears local storage.
 clear.addEventListener("click", function () {
     localStorage.clear();
     location.reload();
 });
-
+// gets local storage info
 var allScores = localStorage.getItem("allScores");
 allScores = JSON.parse(allScores);
 
@@ -19,7 +20,7 @@ if (allScores !== null) {
 
     }
 }
-
+// when go back is pushed you get redirected back to index.html page
 goBack.addEventListener("click", function () {
     window.location.replace("./index.html");
 });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 // DOM referenced variables
-var currentTime = document.querySelector("#currentTime");
-var timer = document.querySelector("#startBtn");
-var wrapper = document.querySelector("#wrapper");
-var questionsDiv = document.querySelector("#questionsDiv");
+var timerDiv = document.querySelector("#timerDiv");
+var timerStart = document.querySelector("#startBtn");
+var main = document.querySelector("#main");
+var questionsContainer = document.querySelector("#questionsContainer");
 
 
 // Declared Variables
@@ -10,7 +10,7 @@ var deduction = 10;
 var questionIndex = 0;
 var score = 0;
 var secondsLeft = 76;
-var holdInterval = 0;
+var hold = 0;
 
 
 // Creates new unordered list
@@ -50,44 +50,48 @@ var timeEl = document.querySelector("#time")
 var time = questions.length * 15;
 var timerId;
 
-timer.addEventListener("click", function () {
+// Determines how much time is left if 0 quiz ends.
+timerStart.addEventListener("click", function () {
 
-    if (holdInterval === 0) {
-        holdInterval = setInterval(function () {
+    if (hold === 0) {
+        hold = setInterval(function () {
             secondsLeft--;
-            currentTime.textContent = "Time: " + secondsLeft;
+            timerDiv.textContent = "Time: " + secondsLeft;
 
             if (secondsLeft <= 0) {
-                clearInterval(holdInterval);
+                clearInterval(hold);
                 gameOver();
-                currentTime.textContent = "Out of time!";
+                timerDiv.textContent = "Out of time!";
             }
         }, 1000);
     }
-    render(questionIndex);
+    retrieve(questionIndex);
 });
-function render(questionIndex) {
 
-    questionsDiv.innerHTML = "";
+//determines which question needs to be presented
+function retrieve(questionIndex) {
+
+    questionsContainer.innerHTML = "";
     ulCreate.innerHTML = "";
 
     for (var i = 0; i < questions.length; i++) {
 
         var userQuestion = questions[questionIndex].question;
         var userChoices = questions[questionIndex].choices;
-        questionsDiv.textContent = userQuestion;
+        questionsContainer.textContent = userQuestion;
     }
 
     userChoices.forEach(function (newItem) {
         var listItem = document.createElement("li");
         listItem.textContent = newItem;
-        questionsDiv.appendChild(ulCreate);
+        questionsContainer.appendChild(ulCreate);
         ulCreate.appendChild(listItem);
-        listItem.addEventListener("click", (compare));
+        listItem.addEventListener("click", (verifier));
     })
 }
 
-function compare(event) {
+// determines if user is right or wrong depending on answer picked.
+function verifier(event) {
     var element = event.target;
 
     if (element.matches("li")) {
@@ -109,56 +113,57 @@ function compare(event) {
         gameOver();
         createDiv.textContent = "Good Job!"
     } else {
-        render(questionIndex);
+        retrieve(questionIndex);
     }
-    questionsDiv.appendChild(createDiv);
+    questionsContainer.appendChild(createDiv);
 }
 
+// when quiz is over user will be notified of score and issued a prompt to enter initials. this gets stored on local storage.
 function gameOver() {
-    questionsDiv.innerHTML = "";
-    currentTime.innerHTML = "";
-
+    questionsContainer.innerHTML = "";
+    timerDiv.innerHTML = "";
+//the next 5 vars are creating corresponding elements and filling them with text content.
     var createH1 = document.createElement("h1");
     createH1.setAttribute("id", "createH1");
     createH1.textContent = "Quiz Over!"
 
-    questionsDiv.appendChild(createH1);
+    questionsContainer.appendChild(createH1);
 
 
     var createP = document.createElement("p");
     createP.setAttribute("id", "createP");
 
-    questionsDiv.appendChild(createP);
+    questionsContainer.appendChild(createP);
 
     if (secondsLeft >= 0) {
         var timeLeft = secondsLeft;
         var createP2 = document.createElement("p");
-        clearInterval(holdInterval);
+        clearInterval(hold);
         createP.textContent = "Your score is: " + timeLeft;
 
-        questionsDiv.appendChild(createP2);
+        questionsContainer.appendChild(createP2);
     }
 
     var createLabel = document.createElement("label");
     createLabel.setAttribute("id", "createLabel");
     createLabel.textContent = "Enter Your Initials: ";
 
-    questionsDiv.appendChild(createLabel);
+    questionsContainer.appendChild(createLabel);
 
     var createInput = document.createElement("input");
     createInput.setAttribute("type", "text");
     createInput.setAttribute("id", "initials");
     createInput.textContent = "";
 
-    questionsDiv.appendChild(createInput);
+    questionsContainer.appendChild(createInput);
 
     var createSubmit = document.createElement("button");
     createSubmit.setAttribute("type", "submit");
     createSubmit.setAttribute("id", "Submit");
     createSubmit.textContent = "Submit"
 
-    questionsDiv.appendChild(createSubmit);
-
+    questionsContainer.appendChild(createSubmit);
+// when submit is clicked scores get stored on local storage if no initials were entered it says "no initials entered"
     createSubmit.addEventListener("click", function () {
         var initials = createInput.value;
 
@@ -181,8 +186,6 @@ function gameOver() {
             allScores.push(userScore);
             var newScore = JSON.stringify(allScores);
             localStorage.setItem("allScores", newScore);
-            
-
         }
     });
 }
